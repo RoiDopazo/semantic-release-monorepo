74a33649b146f932a227cb425c7728382f33b4f5
Roi Dopazo <RoiDopazo@users.noreply.github.com>
2022-06-24 16:34:28 +0200

    Update only-package-commits.js

--- src/only-package-commits.js
+++ src/only-package-commits.js
@@ -6,6 +6,7 @@ const pLimit = require('p-limit');
 const debug = require('debug')('semantic-release:monorepo');
 const { getCommitFiles, getRoot } = require('./git-utils');
 const { mapCommits } = require('./options-transforms');
+const fs = require('fs');
 
 const memoizedGetCommitFiles = memoizeWith(identity, getCommitFiles);
 
@@ -31,34 +32,54 @@ const withFiles = async commits => {
   );
 };
 
-const onlyPackageCommits = async commits => {
-  const packagePath = await getPackagePath();
-  debug('Filter commits by package path: "%s"', packagePath);
+const onlyPackageCommits = async (analyzeLinkedDependencies, commits) => {
+  let packagesIncluded = [];
+
+  if (analyzeLinkedDependencies) {
+    const gitRoot = await getRoot();
+    const packagesDir = fs.readdirSync(path.resolve(gitRoot, analyzeLinkedDependencies.dir));
+  
+    const packageJson = JSON.parse(fs.readFileSync(path.resolve(gitRoot, await getPackagePath(), 'package.json'), 'utf8'));
+
+    packagesIncluded = Object.keys(packageJson.dependencies)
+      .filter(dependency => packagesDir.includes(dependency))
+      .map(package => path.join(analyzeLinkedDependencies.dir, package));
+  }
+  
+  const packagePaths = [await getPackagePath(), ...packagesIncluded];
+  console.log('packagePath: ', packagePaths);
+
+  debug('Filter commits by package path: "%s"', packagePaths);
   const commitsWithFiles = await withFiles(commits);
   // Convert package root path into segments - one for each folder
-  const packageSegments = packagePath.split(path.sep);
 
-  return commitsWithFiles.filter(({ files, subject }) => {
-    // Normalise paths and check if any changed files' path segments start
-    // with that of the package root.
-    const packageFile = files.find(file => {
-      const fileSegments = path.normalize(file).split(path.sep);
-      // Check the file is a *direct* descendent of the package folder (or the folder itself)
-      return packageSegments.every(
-        (packageSegment, i) => packageSegment === fileSegments[i]
-      );
+  const commitsArray = packagePaths.map(packagePath => {
+    const packageSegments = packagePath.split(path.sep);
+
+    return commitsWithFiles.filter(({ files, subject }) => {
+      // Normalise paths and check if any changed files' path segments start
+      // with that of the package root.
+      const packageFile = files.find(file => {
+        const fileSegments = path.normalize(file).split(path.sep);
+        // Check the file is a *direct* descendent of the package folder (or the folder itself)
+        return packageSegments.every(
+          (packageSegment, i) => packageSegment === fileSegments[i]
+        );
+      });
+  
+      if (packageFile) {
+        debug(
+          'Including commit "%s" because it modified package file "%s".',
+          subject,
+          packageFile
+        );
+      }
+  
+      return !!packageFile;
     });
+  })
 
-    if (packageFile) {
-      debug(
-        'Including commit "%s" because it modified package file "%s".',
-        subject,
-        packageFile
-      );
-    }
-
-    return !!packageFile;
-  });
+  return commitsArray.flat();
 };
 
 // Async version of Ramda's `tap`
@@ -77,13 +98,14 @@ const logFilteredCommitCount = logger => async ({ commits }) => {
   );
 };
 
-const withOnlyPackageCommits = plugin => async (pluginConfig, config) => {
+const withOnlyPackageCommits = (plugin) => async (pluginConfig, config) => {
+  console.log('pluginConfig: ', pluginConfig);
   const { logger } = config;
 
   return plugin(
     pluginConfig,
     await pipeP(
-      mapCommits(onlyPackageCommits),
+      mapCommits((commits) => onlyPackageCommits(pluginConfig.analyzeLinkedDependencies, commits)),
       tapA(logFilteredCommitCount(logger))
     )(config)
   );
